0faa92b42e9feefb1daf66fa6c473e9e6bfa65c7
Hannes Schröder <hannes.schroeder@outlook.com>
2020-06-18 11:12:08 +0200

    add cors rules

--- restapi-feed/src/server.ts
+++ restapi-feed/src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import { sequelize } from './sequelize';
 
@@ -19,11 +20,11 @@ const c = config.dev;
   app.use(bodyParser.json());
 
   //CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+  app.use(cors({
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization"],
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    origin: c.url
+  }));
 
   app.use('/api/v0/', IndexRouter)
 
